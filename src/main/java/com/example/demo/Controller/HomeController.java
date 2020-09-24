9ebf402361d1d98cd7a1b0927c2e9ff5a70bde90
BenjiHasCode <57215161+BenjiHasCode@users.noreply.github.com>
2020-09-24 20:11:54 +0200

    Added update and create product pages :D

--- src/main/java/com/example/demo/Controller/HomeController.java
+++ src/main/java/com/example/demo/Controller/HomeController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -24,9 +26,31 @@ public class HomeController {
         return "index.html";
     }
 
+    @GetMapping("/createProduct")
+    public String createProduct(){
+        return "createProduct";
+    }
+
+    @PostMapping("/createProduct")
+    public String createProduct(@ModelAttribute Product p){
+        productService.create(p);
+        return "redirect:/";
+    }
+
     @GetMapping("/deleteProduct/{id}")
     public String deleteProduct(@PathVariable("id") int id){
         productService.delete(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/updateProduct/{id}")
+    public String updateProduct(@PathVariable("id") int id, Model model){
+        model.addAttribute("product", productService.read(id));
+        return "updateProduct";
+    }
+    @PostMapping("/updateProduct")
+    public String updateProduct(@ModelAttribute Product p){
+        productService.update(p);
         return "redirect:/";
     }
 }
