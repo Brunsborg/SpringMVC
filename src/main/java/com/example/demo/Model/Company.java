83bc75391bfcfb2fdffdbb041e1fce3af2956424
BenjiHasCode <57215161+BenjiHasCode@users.noreply.github.com>
2020-11-05 09:58:14 +0100

    made company and category

---
+++ src/main/java/com/example/demo/Model/Company.java
@@ -0,0 +1,38 @@
+package com.example.demo.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+public class Company {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private long id;
+    private String name;
+
+    public Company(){}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String toString(){
+        return "ID: " + id +
+                "\nName: " +name;
+    }
+}
